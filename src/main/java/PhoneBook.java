71830853f7bb48bf94821ea06dcef22893713b1a
Ganzyuk Nikita <nganzyuk.edu@gamail.com>
2023-05-05 01:14:22 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    Map<String, String> phoneBook;
+    public PhoneBook(){
+        phoneBook = new HashMap<>();
+    }
     public int add (String name, String number){
-        return 0;
+        this.phoneBook.put(name, number);
+        return this.phoneBook.size();
     }
 }
